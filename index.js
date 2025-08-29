adb1f4c420c7181d8321109c953e4a4095d321b8
Yu <twodot40@gmail.com>
2025-08-29 11:46:11 +0100

    lff

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.APP_PORT || 3000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRoutes = require("./src/routes/userRoutes");
-const billsPaymentRoutes = require("./src/routes/billsPaymentRoutes");
+// const billsPaymentRoutes = require("./src/routes/billsPaymentRoutes");
 const passport = require('./config/passport');
 const oauthRoutes = require('./src/routes/authRoutes');
 app.use(bodyParser.json());
@@ -23,7 +23,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use('/auth', oauthRoutes);
 app.use("/api/v1/user", userRoutes)
-app.use("/api/v1/utility-bills", billsPaymentRoutes)
+// app.use("/api/v1/utility-bills", billsPaymentRoutes)
 
 
 
